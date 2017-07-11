788986a701e1b9a2e29705a0f6b3dc56d9c3531d
koskh <koskh@yandex.ru>
2017-07-11 13:16:46 +0200

    #master utilities/number/extract

---
+++ mocha.setup.js
@@ -0,0 +1,12 @@
+require('babel-core/register');
+// require('mock-local-storage');
+
+const mockCssModules = require('mock-css-modules');
+mockCssModules.register(['.css', '.scss', '.pcss']);
+
+['.png', '.jpg'].forEach(ext => {
+    require.extensions[ext] = () => null;
+});
+
+global.window = {};
+global.PROJECT_ENV = 'debug';
