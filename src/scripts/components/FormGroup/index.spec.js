5d87b9d130b80d25be03fcc15443e4f8fc8d6dbe
koskh <koskh@yandex.ru>
2017-07-15 00:28:17 +0200

    #master FormGrooup тесты

--- src/scripts/components/FormGroup/index.spec.js
+++ src/scripts/components/FormGroup/index.spec.js
@@ -8,7 +8,13 @@ import { stateClasses, Components } from './shared';
 
 import FormGroup from '.';
 
-describe('components/FormGroup: common', () => {
+const NodeName = 'NodeName';
+const ComponentValue = 'Value';
+const WrongValidationState = 'abrakadabra';
+const WrongType = 'abrakadabra';
+const DefaultComponent = Components.input;
+
+describe('components/FormGroup: Component', () => {
     it('default renders without errors', () => {
         expect(shallow(<FormGroup />)).to.have.length(1);
     });
@@ -19,25 +25,45 @@ describe('components/FormGroup: common', () => {
         });
     });
 
-    it('validationState has not found ', () => {
-        expect(shallow(<FormGroup validationState="abrakadabra" />)).to.have.length(1);
+    it('unknown validationState is not rendered', () => {
+        expect(shallow(<FormGroup validationState={WrongValidationState} />).find(`.form-group.${WrongValidationState}`)).to.have.length(0);
     });
 
 
-    it('render feedbackText ', () => {
-        expect(shallow(<FormGroup feedbackText="abrakadabra" />).contains(<div className="form-control-feedback">abrakadabra</div>)).to.equal(true);
+    it('feedbackText is rendered', () => {
+        expect(shallow(<FormGroup feedbackText="feedbackText" />).contains(<div className="form-control-feedback">feedbackText</div>)).to.equal(true);
     });
 });
 
 describe('components/FormGroup: children', () => {
     it('renders Input as default ', () => {
         expect(shallow(<FormGroup />)).to.have.length(1);
-        expect(shallow(<FormGroup />).find(Components.input)).to.have.length(1);
+        expect(shallow(<FormGroup />).find(DefaultComponent)).to.have.length(1);
+    });
+
+    it('renders all type Components', () => {
+        _.each(Components, (v, k) => {
+            expect(shallow(<FormGroup type={k} />).find(Components[k])).to.have.length(1);
+        });
     });
 
     it('throw new Error if has not right type children', () => {
         expect(() => {
-            shallow(<FormGroup type="abrakadabra" />);
+            shallow(<FormGroup type={WrongType} />);
         }).to.throw();
+    });
+
+    it('simulates onChange events on default Input', () => {
+        const onChange = sinon.spy();
+        shallow(<FormGroup onChange={onChange} />).find(DefaultComponent).simulate('change',  { target: { value: ComponentValue } });
+        expect(onChange.calledWith(null, ComponentValue)).to.equal(true);
+    });
+
+    it('simulates onChange events on all type  Components', () => {
+        const onChange = sinon.spy();
+        _.each(Components, (v, k) => {
+            shallow(<FormGroup type={k} onChange={onChange} name={NodeName} />).find(Components[k]).simulate('change',  { target: { value: ComponentValue } });
+            expect(onChange.calledWith(NodeName, ComponentValue)).to.equal(true);
+        });
     });
 });
