b39f778108b6ba7647aeb1366dd80e873e0639c8
koskh <koskh@yandex.ru>
2017-07-25 11:53:45 +0200

    #master server разделение api

--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const path = require('path');
 
-const api = require('./api');
-
-
 // const webpackMiddleware = require('webpack-dev-middleware');
 // const webpack = require('webpack');
 // const configWebpack = require('../webpack/webpack.config');
 // const compiler = webpack(configWebpack);
+
+const api = require('./api');
 
 const app = express();
 const port = 3000;
@@ -23,16 +22,8 @@ const publicPath = path.resolve(__dirname, '../build');
 //     noInfo: false,
 //     publicPath: '/',
 // }));
-
 // app.use(require("webpack-hot-middleware")(compiler));
 
-// app.get('/api/references', (request, response) => {
-//     response.writeHead(200, { 'Content-Type': 'application/json' });
-//     setTimeout(() => {
-//         response.end(JSON.stringify({ test1: ['test1', 'test2', 'test3'], test2: ['test21', 'test22', 'test23'], test3: ['test31', 'test32', 'test33'] }));
-//     }, 2000);
-//     // response.end(JSON.stringify(['test1', 'test2', 'test3']));
-// });
 
 app.use('/api', api);
 
