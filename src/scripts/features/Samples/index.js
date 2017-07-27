a863a08190c9b7f54a15c348cd8e074654c772d2
koskh <koskh@yandex.ru>
2017-07-28 01:24:32 +0200

    #mastereslintrc react/jsx-boolean-value

--- src/scripts/features/Samples/index.js
+++ src/scripts/features/Samples/index.js
@@ -15,13 +15,13 @@ export default class Home extends React.Component {
             <h1>Примеры компонентов</h1>
             <div className="row align-items-center">
               <div className="col-2"> Идикатор загрузки</div>
-              <div className="col-3"><PendingIndicator pending /></div>
+              <div className="col-3"><PendingIndicator pending={true} /></div>
               <div className="col-3"><PendingIndicator pending={false}>Fetching is done.</PendingIndicator></div>
               <div className="col-3"><PendingIndicator pending={false} errorMessage="Ошибка получения данных">Fetching is done.</PendingIndicator></div>
             </div>
             <div className="row align-items-center">
               <div className="col-2"> Идикатор загрузки в действии</div>
-              <div className="col-3"><PendingIndicator Indicator={Spiner} pending /></div>
+              <div className="col-3"><PendingIndicator Indicator={Spiner} pending={true} /></div>
             </div>
           </article>
         );
