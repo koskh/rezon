8e00a00e8e98afa436545fbc74b5e1ac61767bca
koskh <koskh@yandex.ru>
2017-07-28 18:40:17 +0200

    #master feature/Login

--- src/scripts/components/LoginForm/index.js
+++ src/scripts/components/LoginForm/index.js
@@ -5,8 +5,6 @@ import React from 'react';
 
 import styles from './index.pcss';
 
-//eslint-disable-next-line
-
 type Props = {
     // children?: React.Children
 }
@@ -32,19 +30,21 @@ class LoginForm extends React.Component {
         // const { children } = this.props;
 
         return (
-          <form className="form-signin">
-            <h2 className="form-signin-heading">Please sign in</h2>
-            <label htmlFor="inputEmail" className="sr-only">Email address</label>
-            <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required={true} />
-            <label htmlFor="inputPassword" className="sr-only">Password</label>
-            <input type="password" id="inputPassword" className="form-control" placeholder="Password" required={true} />
-            <div className="checkbox">
-              <label className="checkbox" htmlFor="rememberMe">
-                <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe" /> Remember me
-              </label>
-            </div>
-            <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-          </form>
+          <div className={styles.wrapper}>
+            <form className={styles['form-signin']}>
+              <h2 className={styles['form-signin-heading']}>Please sign in</h2>
+              <label htmlFor="inputEmail" className="sr-only">Email address</label>
+              <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required={true} />
+              <label htmlFor="inputPassword" className="sr-only">Password</label>
+              <input type="password" id="inputPassword" className="form-control" placeholder="Password" required={true} />
+              <div className="checkbox">
+                <label className="checkbox" htmlFor="rememberMe">
+                  <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe" /> Remember me
+                        </label>
+              </div>
+              <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+            </form>
+          </div>
         );
     }
 }
