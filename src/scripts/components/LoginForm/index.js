cbbd54b0fd1baaa34e890982957df6d231bf30b7
koskh <koskh@yandex.ru>
2017-08-10 00:14:27 +0500

    #master разворачивание, first step

--- src/scripts/components/LoginForm/index.js
+++ src/scripts/components/LoginForm/index.js
@@ -3,12 +3,12 @@
 import { connect } from 'react-redux';
 
 // import reset from './store/actions/reset';
-import {makeLogin, cancelLogin} from './store/actions/login';
+import { makeLogin, cancelLogin } from './store/actions/login';
 
 import LoginForm from './_components/';
 
-function mapStateToProps({authUser, router}: State) {
-    return { authUser, router};
+function mapStateToProps({ authUser, router }: State) {
+    return { authUser, router };
 }
 
 export default connect(mapStateToProps, { makeLogin, cancelLogin })(LoginForm);
